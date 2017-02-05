aea1a03d7dcc08d23e7ba8625b7d5210704e4e51
Michał Wyszomirski <bimgissql@users.noreply.github.com>
2017-02-05 22:33:42 +0100

    Add files via upload

---
+++ Redis/execute.r
@@ -0,0 +1,21 @@
+# Require rredis package
+library("rredis")
+
+# Connect to Redis server
+redisConnect(host="127.0.0.1", port=6379)
+
+# Set key/value
+redisSet("x1",123)
+
+# Set y to value of x1 
+y <- redisGet("x1")
+
+# Show y
+print(y)
+
+# Pass any command to Redis
+redisCmd('set','x2','456')
+redisCmd('get','x2')
+
+# Close connection to Redis
+redisClose()
