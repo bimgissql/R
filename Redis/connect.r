d5e3ef6e4004a537aea0bc8202bbfe7608863efe
Michał Wyszomirski <bimgissql@users.noreply.github.com>
2017-02-05 22:33:00 +0100

    Update connect.r

--- Redis/connect.r
+++ Redis/connect.r
@@ -4,23 +4,12 @@ library("rredis")
 # Connect to Redis server
 redisConnect(host="127.0.0.1", port=6379)
 
-# Set key/value
-redisSet("x1",123)
+# Run dbsize command on Redis
+x <- redisCmd('dbsize')
 
-# Get x
-redisGet("x1")
-
-# Set y to value of x 
-y <- redisGet("x1")
-
-# Show y
-print(y)
-
-# Pass any command to Redis
-redisCmd('set','x2','456')
-redisCmd('get','x2')
+# Print result
+print(x)
 
 # Close connection to Redis
 redisClose()
-
 
